1baa2920937dfec2d5bc98c813aadcf5ae2e9940
Kemal Yildirim <kemalyildirm@gmail.com>
2023-02-11 09:46:45 +0300

    flyway db migration impl for h2

---
+++ src/main/resources/db/migration/h2/V1.2__insert_data_to_bookmarks.sql
@@ -0,0 +1,5 @@
+insert into bookmarks (title, url, created_at) values
+
+('Google', 'www.google.com.tr', CURRENT_TIMESTAMP()),
+('Twitter', 'www.twitter.com', CURRENT_TIMESTAMP()),
+('Reddit', 'www.reddit.com', CURRENT_TIMESTAMP());
